463396c42880892f79de0192d208abfb21541650
Luis Carlos de la Torre Flores <koreano_2519@hotmail.com>
2023-06-23 12:53:45 -0600

    seccion 3 terminada

--- 03-counter-app-vite/tests/CounterApp.test.jsx
+++ 03-counter-app-vite/tests/CounterApp.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { CounterApp } from '../src/CounterApp';
 
 describe('Pruebas en <CounterApp />', () => {
@@ -11,5 +11,28 @@ describe('Pruebas en <CounterApp />', () => {
     test('Debe de mostrar el valor inicial de 100', () => {
         render( <CounterApp value={value} /> );
         expect( screen.getByRole('heading', { level: 2 }).innerHTML ).toContain(value.toString());
+    });
+
+    test('debe de incrementar con el botón +1', () => {
+        render( <CounterApp value={value} /> );
+        fireEvent.click( screen.getByText('+1') );
+        expect( screen.getByText('101') ).toBeTruthy();
+    });
+
+    test('debe de decrementar con el botón -1', () => {
+        render( <CounterApp value={value} /> );
+        fireEvent.click( screen.getByText('-1') );
+        expect( screen.getByText('99') ).toBeTruthy();
+    });
+
+    test('debe de funcionar el boton de reset', () => {
+        render( <CounterApp value={value} /> );
+        fireEvent.click( screen.getByText('+1') );
+        fireEvent.click( screen.getByText('+1') );
+        fireEvent.click( screen.getByText('+1') );
+        // fireEvent.click( screen.getByText('Reset') );
+
+        fireEvent.click(screen.getByRole('button', { name: 'btn-reset'} ));
+        expect( screen.getByText('100') ).toBeTruthy();
     });
  });
